144ca5699e0aafa6848042c23114b859ff8484ca
royalistofficial <mishabygaga1414@gmail.com>
2025-04-27 10:00:15 +0300

    correction

--- lab6/main.py
+++ lab6/main.py
@@ -94,14 +94,15 @@ samples = {20: np.random.normal(0, 1, 20), 100:np.random.normal(0, 1, 100)}
 
 rows_normal = []
 rows_asym = []
+alpha = 0.05
 for n, sample in samples.items():
-    mL, mU, sL, sU = ci_normal(sample)
+    mL, mU, sL, sU = ci_normal(sample, alpha)
     rows_normal.append({
         "n": n,
         "$m$": f"{mL:.2f} < $m$ < {mU:.2f}",
         "$\\sigma$": f"{sL:.2f} < $\\sigma$ < {sU:.2f}"
     })
-    mL2, mU2, sL2, sU2 = ci_asymptotic(sample)
+    mL2, mU2, sL2, sU2 = ci_asymptotic(sample, alpha)
     rows_asym.append({
         "n": n,
         "$m$": f"{mL2:.2f} < $m$ < {mU2:.2f}",
@@ -115,7 +116,7 @@ df_normal.to_latex(
     os.path.join(out_dir, "normal_intervals.tex"),
     index=False,
     column_format="|c|c|c|",
-    caption="Доверительные интервалы для параметров нормального распределения",
+    caption=f"Доверительные интервалы для параметров нормального распределения,  $\\alpha={alpha}$",
     label="tab:normal_intervals",
     escape=False,
     position="H"
@@ -125,7 +126,7 @@ df_asym.to_latex(
     os.path.join(out_dir, "asym_intervals.tex"),
     index=False,
     column_format="|c|c|c|",
-    caption="Доверительные интервалы для параметров произвольного распределения (асимптотический подход)",
+    caption=f"Доверительные интервалы для параметров произвольного распределения (асимптотический подход),  $\\alpha={alpha}$",
     label="tab:asym_intervals",
     escape=False,
     position="H"
